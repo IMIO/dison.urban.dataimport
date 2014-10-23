65f18e25081882b070995bf909b38d147c26fc55
Simon Delcourt <delcourt.simon@gmail.com>
2014-10-23 15:08:29 +0200

    Override settings form to pass the db_name as setting.

--- src/dison/urban/dataimport/settings.py
+++ src/dison/urban/dataimport/settings.py
@@ -1,7 +1,29 @@
 # -*- coding: utf-8 -*-
 
+from imio.urban.dataimport.settings import ImporterSettingsEditForm
+from imio.urban.dataimport.urbaweb.settings import UrbawebImporterFromSettingsForm
 from imio.urban.dataimport.urbaweb.settings import UrbawebImporterSettings
 
 
+class DisonImporterSettingsEditForm(ImporterSettingsEditForm):
+    """ """
+
+
 class DisonImporterSettings(UrbawebImporterSettings):
-    """"""
+    """ """
+    form = DisonImporterSettingsEditForm
+
+
+class DisonImporterFromSettingsForm(UrbawebImporterFromSettingsForm):
+    """ """
+
+    def get_importer_settings(self):
+        """
+        Return the db name to read.
+        """
+
+        args = {
+            'db_name': 'Tab_Urba 97.mdb',
+        }
+
+        return args
